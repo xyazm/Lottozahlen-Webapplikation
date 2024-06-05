ca66c97aa7ba3c076dfc43320a2b14e7039e3f67
xyazm <yassminmohandis@gmail.com>
2024-06-05 23:57:13 +0200

    fertiges design eines lottoschein ohen funktion

--- frontend/src/components/Lottoschein/Lottoschein.js
+++ frontend/src/components/Lottoschein/Lottoschein.js
@@ -22,10 +22,10 @@ export default function Lottoschein() {
 
 return (
   <div className="flex flex-col items-center min-h-screen">
-    <div id="lottschein" className="grid grid-cols-7 gap-2 border-red-500 p-1 flag bg-red-100 border-[1.5px] hover-stift">
+    <div id="lottschein" className="flag grid grid-cols-7 gap-2 border-red-500 p-1 bg-red-100 border-[1.5px] hover-stift relative z-0">
       {numbers.map((row, i) => (
         row.map((num, j) => (
-          <div key={`${i}-${j}`} className="group relative border bg-white border-red-500 text-red-500 p-1 flex items-center justify-center">
+          <div key={`${i}-${j}`} className="group relative border bg-white border-red-500 text-red-500 p-1 flex items-center justify-center z-[-1]">
             {num}
             <div className="absolute bottom-1 right-1 transform opacity-0 group-hover:opacity-100">
               <CrossHandwritten className='fill-black w-4'/>
