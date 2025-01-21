023e6f1dd04ab8a8227fd0a315d3c8a7fd29dc9a
xyazm <yassminmohandis@gmail.com>
2025-01-21 17:45:01 +0100

    neuer datenschutz

--- frontend/src/pages/Datenschutz.js
+++ frontend/src/pages/Datenschutz.js
@@ -31,17 +31,18 @@ export default function Datenschutz() {
       <ul className="body-list mb-6">
         <li>Ihr Name</li>
         <li>Ihre RUB-Mailadresse</li>
-        <li>(eventuell noch Studiengang?)</li>
       </ul>
 
       <h2>Zweck der Datenverarbeitung</h2>
       <p>
         Die genannten Daten werden ausschließlich zum Zweck der Durchführung und Auswertung der Lottoschein-Muster-Analyse im Rahmen meiner Bachelorarbeit verwendet.
+        Personenbezogene Daten werden gespeichert, um den Lernprozess rückverfolgen zu können. Diese Daten können nur vom Administrator eingesehen werden und werden ansonsten 
+        ausschließlich anonymisiert in den Statistiken verarbeitet.
       </p>
 
       <h2>Speicherdauer</h2>
       <p>
-        Die erhobenen Daten werden nach Abschluss der Bachelorarbeit und Veröffentlichung der Ergebnisse gelöscht, spätestens jedoch nach [Angabe einer Frist, z.B. sechs Monate nach Abschluss].
+        Die erhobenen Daten werden nach Abschluss der Bachelorarbeit und Veröffentlichung der Ergebnisse gelöscht, spätestens jedoch nach sechs Monaten.
       </p>
 
       <h2>Rechtsgrundlage</h2>
@@ -56,6 +57,27 @@ export default function Datenschutz() {
 
       <p>
         Bei Fragen zur Verarbeitung Ihrer personenbezogenen Daten oder zur Wahrnehmung Ihrer Rechte wenden Sie sich bitte an den oben genannten Verantwortlichen.
+      </p>
+
+      <h2>Haftungsausschluss</h2>
+      <p>
+        Diese Webanwendung dient ausschließlich Forschungs- und Lehrzwecken. Es handelt sich um eine reine Simulation, und es besteht keinerlei Anspruch auf Gewinne jeglicher Art. 
+        Jede Ähnlichkeit dieser Webanwendung mit anderen Webseiten oder Plattformen ist rein zufällig und nicht beabsichtigt. Der Betreiber der Seite übernimmt keine Haftung für etwaige Missverständnisse oder irrtümliche Nutzungen der Seite.
+      </p>
+
+      <h2>Nutzungsbedingungen</h2>
+      <p>
+        Mit der Nutzung dieser Webanwendung und der ausdrücklichen Akzeptanz der Nutzungsbedingungen erklärt sich der Benutzer mit den folgenden Bedingungen einverstanden:
+      </p>
+      <ul className="body-list mb-6">
+        <li>Die Nutzung der Webanwendung erfolgt ausschließlich zu Lehr- und Forschungszwecken.</li>
+        <li>Es besteht kein Anspruch auf Gewinne oder andere Ansprüche.</li>
+        <li>Personenbezogene Daten, die für die Rückverfolgung des Lernprozesses relevant sind, werden gespeichert.</li>
+        <li>Die gespeicherten Daten können ausschließlich vom Administrator eingesehen werden und werden ansonsten anonymisiert in Statistiken verarbeitet.</li>
+      </ul>
+
+      <p>
+        Durch die Nutzung der Webanwendung stimmen Sie diesen Bedingungen zu. Sollten Sie mit den Bedingungen nicht einverstanden sein, bitten wir Sie, die Anwendung nicht zu nutzen.
       </p>
     </div>
   );
